c9a5c2f8b492cec975e8dcedcc4a48e2513c8b45
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-10 16:54:12 -0600

    added select panel to index.html, changed header, added .fetch() and console.logged to check if working, which it was

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const testSubmit = document.getElementById(search)
-console.log(testSubmit)
+const searchName = document.getElementById("search")
 
-testSubmit.addEventListener("submit", function(e) {
+searchName.addEventListener("submit", function(e) {
     e.preventDefault()
     alert("working!")
 })
+
+fetch("https://api.spacexdata.com/v4/crew")
+.then(res => res.json())
+.then(data => console.log(data))

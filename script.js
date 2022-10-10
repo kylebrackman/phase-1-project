fe0ab8a520b537735bcb96b7d007a69282fe3d7c
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-10 17:30:03 -0600

    added onchange event listener to dropdown, tested by console.logging 'test change', which logged successfully' '

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ searchName.addEventListener("submit", function(e) {
     alert("working!")
 })
 
+const selectAgencyDropdown = document.getElementById("selectAgency")
+console.log(selectAgencyDropdown)
+
+selectAgencyDropdown.onchange = function(){
+    console.log("test change")
+}
+
+
+function changeAgency(){
+}
+
 fetch("https://api.spacexdata.com/v4/crew")
 .then(res => res.json())
 .then(data => console.log(data))

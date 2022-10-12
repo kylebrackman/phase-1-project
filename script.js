fa245cf48b1eb4a74daf75e367c1e8a99153bd5a
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-11 22:01:13 -0600

    displayed crew member names to the DOM when using the select onchange method, now need to filter by agency

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-const searchName = document.getElementById("search")
+const searchName = document.getElementById("search");
 
 searchName.addEventListener("submit", function(e) {
     e.preventDefault()
     alert("working!")
-})
+});
 
-const selectAgencyDropdown = document.getElementById("selectAgency")
+// fetching SpaceX API and attempting to get crew info
+const selectAgencyDropdown = document.getElementById("selectAgency");
+selectAgencyDropdown.onchange = fetchCrewInfo
 
-selectAgencyDropdown.onchange = function(){
+function fetchCrewInfo() {
     fetch("https://api.spacexdata.com/v4/crew")
     .then(res => res.json())
-    .then(data => changeAgency(data))
+    .then(data => renderCrewNames(data))
+};
+
+function renderCrewNames(crew) {
+    const main = document.querySelector("main")
+    crew.forEach(crew => {
+        const h2 = document.createElement("h2")
+        h2.innerHTML = crew.name
+        main.appendChild(h2)
+    })
 }
 
-function changeAgency(data){
-    let p = document.createElement("p")
-    p.innerText = data.name 
-    document.querySelector("#crewMembers").appendChild(p)
-}
+
+// function changeAgency(data){
+//     let p = document.createElement("p")
+//     crewMemberName = data.filter(filterCrew(data))
+//     p.innerText = crewMemberName
+//     document.querySelector("#crewMembers").appendChild(p)
+// };
+
 
 let darkMode = document.getElementById("darkMode");
 darkMode.addEventListener("click", changeColorScheme);
-
 let dark = false;
-
 function changeColorScheme() {
     dark = !dark
     darkMode.innerText = "Light Mode"
-    document.body.classList.toggle("darkMode");
+    document.body.classList.toggle("darkMode")
     if (!dark) {
         darkMode.innerText = "Dark Mode"
     }
-}
+};
 
-// function filterCrew(agencyName) {
-//     let crew = data.filter(data => data.agency === "NASA")
-//     console.log(nasaCrew)
-// }
-
-
-
-// fetch("https://api.spacexdata.com/v4/crew")
-// .then(res => res.json())
-// .then(data => console.log(data))

455d37a709263aed7c8a022ebe71172956ac0ad9
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-11 22:39:26 -0600

    added if statements to see if the crewmember had a certain agency, only appended to dom if agency matched the crewmember's agency

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ searchName.addEventListener("submit", function(e) {
 
 // fetching SpaceX API and attempting to get crew info
 const selectAgencyDropdown = document.getElementById("selectAgency");
+
 selectAgencyDropdown.onchange = fetchCrewInfo
 
 function fetchCrewInfo() {
@@ -15,22 +16,65 @@ function fetchCrewInfo() {
     .then(data => renderCrewNames(data))
 };
 
+
 function renderCrewNames(crew) {
     const main = document.querySelector("main")
-    crew.forEach(crew => {
-        const h2 = document.createElement("h2")
-        h2.innerHTML = crew.name
-        main.appendChild(h2)
-    })
+    if (selectAgencyDropdown.value === "NASA" ) {
+        crew.forEach(crew => {
+            if (crew.agency === "NASA") {
+            const h2 = document.createElement("h2")
+            h2.innerHTML = crew.name
+            main.appendChild(h2)
+            }
+        })
+    }
+    if (selectAgencyDropdown.value === "ESA" ) {
+        crew.forEach(crew => {
+            if (crew.agency === "ESA") {
+            const h2 = document.createElement("h2")
+            h2.innerHTML = crew.name
+            main.appendChild(h2)
+            }
+        })
+    }
+    if (selectAgencyDropdown.value === "JAXA" ) {
+        crew.forEach(crew => {
+            if (crew.agency === "JAXA") {
+            const h2 = document.createElement("h2")
+            h2.innerHTML = crew.name
+            main.appendChild(h2)
+            }
+        })
+    }
+    if (selectAgencyDropdown.value === "SpaceX" ) {
+        crew.forEach(crew => {
+            if (crew.agency === "SpaceX") {
+            const h2 = document.createElement("h2")
+            h2.innerHTML = crew.name
+            main.appendChild(h2)
+            }
+        })
+    }
+    if (selectAgencyDropdown.value === "Axiom Space" ) {
+        crew.forEach(crew => {
+            if (crew.agency === "Axiom Space") {
+            const h2 = document.createElement("h2")
+            h2.innerHTML = crew.name
+            main.appendChild(h2)
+            }
+        })
+    }
+    if (selectAgencyDropdown.value === "Roscosmos" ) {
+        crew.forEach(crew => {
+            if (crew.agency === "Roscosmos") {
+            const h2 = document.createElement("h2")
+            h2.innerHTML = crew.name
+            main.appendChild(h2)
+            }
+        })
+    }
+
 }
-
-
-// function changeAgency(data){
-//     let p = document.createElement("p")
-//     crewMemberName = data.filter(filterCrew(data))
-//     p.innerText = crewMemberName
-//     document.querySelector("#crewMembers").appendChild(p)
-// };
 
 
 let darkMode = document.getElementById("darkMode");

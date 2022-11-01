30907cda1cf7486e172fef1eff5c3ea1c57fe03c
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-31 20:11:28 -0600

    refactored, renderCrewData is no longer within the fetch request

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 const spaceXData = "https://api.spacexdata.com/v4/crew"
 const selectAgencyDropdown = document.getElementById("selectAgency");
-selectAgencyDropdown.addEventListener("change", fetchCrewData)
+selectAgencyDropdown.addEventListener("change", renderCrewInfo)
 const darkModeButton = document.getElementById("darkMode");
 darkModeButton.addEventListener("click", changeColorScheme);
 let darkMode = false;
+let savedCrewData = null
 
 function fetchCrewData() {
-    let savedCrewData = null
-    if (savedCrewData === null) {
-        fetch(spaceXData)
-            .then(res => res.json())
-            .then(data => {
-                let savedCrewData = data
-                // this is passing savedCrewData to the above variable up top, then can be used below
-                // in the else statement?
-                renderCrewInfo(savedCrewData)
-            })
-    }
-    else {
-        renderCrewInfo(savedCrewData)
-    }
-};
+    fetch(spaceXData)
+        .then(res => res.json())
+        .then(data => {
+            savedCrewData = data
+        })
+}
 
-function renderCrewInfo(crew) {
+// remember to change paramater name line 30 (crew change)
+
+function renderCrewInfo(event) {
     const main = document.querySelector("main")
     main.innerHTML = ""
     const selectedAgency = selectAgencyDropdown.value
-    crew.forEach(crew => {
+    savedCrewData.forEach(crew => {
         if (crew.agency === selectedAgency) {
             const h2 = document.createElement("h2")
             const img = document.createElement("img")
@@ -34,8 +28,8 @@ function renderCrewInfo(crew) {
             aTag.href = crew.wikipedia
             aTag.innerText = `${crew.name + " wikepedia link!"}`
             img.src = `${crew.image}`
-            img.addEventListener("mouseover", () => {img.classList.toggle("imgMouseOver")})
-            img.addEventListener("mouseout", () => {img.classList.toggle("imgMouseOver")})
+            img.addEventListener("mouseover", () => { img.classList.toggle("imgMouseOver") })
+            img.addEventListener("mouseout", () => { img.classList.toggle("imgMouseOver") })
             h2.innerHTML = crew.name
             main.appendChild(h2)
             main.appendChild(img)
@@ -54,4 +48,6 @@ function changeColorScheme() {
         darkModeButton.innerText = "Light Mode"
     }
 };
+
+fetchCrewData()
 
